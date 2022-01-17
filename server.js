860b27386cbb81d2a8d3b15c72407f09eddb188c
MichValenz <michvalenz27@gmail.com>
2022-01-16 20:10:55 -0600

    added fetch to GET animal resquests

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static("public"));
+
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   // Note that we save the animalsArray as filteredResults here:
@@ -56,6 +58,7 @@ function filterByQuery(query, animalsArray) {
 }
 
 function findById(id, animalsArray) {
+  console.log(animalsArray, id)
   const result = animalsArray.filter((animal) => animal.id === id)[0];
   return result;
 }
@@ -103,6 +106,26 @@ app.get("/api/animals/:id", (req, res) => {
   }
 });
 
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+
+
+
 app.post("/api/animals", (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
@@ -115,6 +138,9 @@ app.post("/api/animals", (req, res) => {
     res.json(animal);
   }
 });
+
+
+
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
